06516f1822c78a218dce80413763af1ae91ecdf0
tk <trishank01@gmail.com>
2026-05-17 23:03:20 +0530

    testing pipeline:v3

--- src/components/layout/AuthLayout.jsx
+++ src/components/layout/AuthLayout.jsx
@@ -5,7 +5,7 @@ const AuthLayout = ({ children }) => {
   return (
     <div className="flex">
       <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12">
-        <h2 className="">Task Manager V7</h2>
+        <h2 className="">Task Manager V8</h2>
         {children}
       </div>
       <div className="hidden md:flex w-[40vw] h-screen items-center justify-center bg-blue-50 bg-cover bg-no-repeat bg-center overflow-hidden p-8">
